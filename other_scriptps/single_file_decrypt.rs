d7f78964f412b9eb7577ab27ace3f75e16a1f01d
c-sleuth <c_sl3uth@proton.me>
2024-03-14 16:12:57 +0000

    Fixed error in single_file_decrypt

---
+++ other_scriptps/single_file_decrypt.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::path::Path;
+
+fn decrypt_file(filename: &Path) -> io::Result<()> {
+    let mut fp = File::open(filename)?;
+    
+    let mut buf = vec![0; 64];
+    fp.read_exact(&mut buf)?;
+    //let bytes_read = fp.read(&mut buf);
+    //buf.resize(bytes_read?, 0); // buf currently has the encrypted data
+    let xor_buf: Vec<u8> = buf.iter().map(|&b| b ^ 0x42).collect();
+    
+    let mut rest_of_file = Vec::new();
+    fp.read_to_end(&mut rest_of_file)?;
+    
+    let mut output = File::create(filename.file_name().unwrap())?;
+    output.write_all(&xor_buf)?;
+    output.write_all(&rest_of_file)?;
+   Ok(()) 
+}
+
+fn main() {
+    let filename = Path::new( /* file path here */ );
+    match decrypt_file(filename) {
+        Ok(bytes) => println!("{:?}", bytes),
+        Err(e) => eprint!("Error: {:?}", e),
+    }
+}
+
